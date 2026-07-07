b58bba0b144def9c82bcced37293495f18aafb2d
coding-astrounaut <gorrilamansad@gmail.com>
2026-07-07 20:02:36 +0100

    main order logic complete

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import menuArray from './data.js'
 
 //number of items in the order
-let pizzaCount = 0
-let hamburgerCount = 0
-let beerCount = 0
+let orders = []
 let totalPrice = 0
 
 document.addEventListener('click', handleClick)
@@ -11,8 +9,9 @@ document.addEventListener('click', handleClick)
 function handleClick(e) {
     if(e.target.dataset.add){
         handleAddToOrder(e.target.dataset.id)
-        console.log(`Pizza: ${pizzaCount}, Hamburger: ${hamburgerCount}, Beer: ${beerCount}, Total Price: $${totalPrice}`)
-    }    
+    }else if (e.target.dataset.remove){
+        handleRemoveFromOrder(e.target.dataset.item)
+    }
 }
 
 function generateMenuElements(menu) {
@@ -28,7 +27,7 @@ function generateMenuElements(menu) {
                     <p class="menu-item-price">$${food.price}</p>
                 </div>
             </div>
-            <button class="add-to-order-btn" data-id="${food.id}" data-add="1">+</button>
+            <button class="add-to-order-btn" data-id="${food.id}" data-add="true">+</button>
         </div>
         `
     })
@@ -40,10 +39,15 @@ function renderMenu() {
 }
 
 function renderOrder() {
+    const order = document.getElementById('payment')
     if(totalPrice){
         // Render order details
-        const order = document.getElementById('payment')
         order.style.display = 'block'
+        renderAddedItems(generatePriceComp())
+        RenderTotalPrice()
+        
+    } else {
+        order.style.display = 'none'
     }
 }
 
@@ -51,24 +55,49 @@ function handleAddToOrder(foodId) {
 
     const food = menuArray.find(item => item.id === Number(foodId))
 
-    if (food) {
-        switch (food.name) {
-            case 'Pizza':
-                pizzaCount++
-                break
-            case 'Hamburger':
-                hamburgerCount++
-                break
-            case 'Beer':
-                beerCount++
-                break
-        }
-        totalPrice += food.price
-    }
+    const foodObj = {name: food.name, price: food.price, id: orders.length}
+    orders.push(foodObj)
+    calcTotalPrice()
     renderOrder()
 }
 
-function handleBasket() {
+function renderAddedItems(html) {
+    document.getElementById('items').innerHTML = html
+}
+
+function calcTotalPrice(){
+    totalPrice = orders.reduce((total, currentItem) => total += currentItem.price, 0)
+}
+
+function RenderTotalPrice(){
+    document.getElementById('total').innerHTML = `
+        <div class='price-line'>
+            <h3 class='price-name'>Total price:</h3>
+            <h4 class='order-item-price'>$${totalPrice}</h4>
+        </div>
+    `
+}
+
+function handleRemoveFromOrder(itemId) {
+    const index = orders.indexOf(orders.find(item => item.id == itemId))
+    if (index > -1) { // only splice array when item is found
+        orders.splice(index, 1); // 2nd parameter means remove one item only
+    }
+    calcTotalPrice()
+    renderOrder()
+}
+
+function generatePriceComp() {
+    return orders.map(item => {return `<div class='price-line'>
+                <div class='price-item'>
+                    <h3 class='price-name'>${item.name}</h3>
+                    <button class='price-remove-btn' data-item='${item.id}' data-remove="true">remove</button>
+                </div>
+                <h4 class='order-item-price'>$${item.price}
+            </div>`}).join('')
+}
+
+function handleBasketRender() {
 
 }
 
